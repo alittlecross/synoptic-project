1ee290af935e0818fc1a1854933244287a9aa273
alittlecross <alittlecross@live.co.uk>
2019-11-12 09:54:07 +0000

    basic wdio setup

---
+++ test/wdio/config.js
@@ -0,0 +1,28 @@
+exports.config = {
+
+  baseUrl: 'http://localhost:3000',
+
+  capabilities: [ { browserName: 'chrome' } ],
+
+  debug: true,
+
+  framework: 'mocha',
+
+  logLevel: 'error',
+
+  maxInstances: 1,
+
+  mochaOpts: { timeout: 300000 },
+
+  reporters: [ 'spec' ],
+
+  seleniumLogs: './.selenium_output',
+
+  services: ['selenium-standalone'],
+
+  specs: [ './test/wdio/features/*.js' ],
+
+  before: (capabilities, specs) => {
+    browser.setWindowSize(1600, 1000)
+  }
+}
